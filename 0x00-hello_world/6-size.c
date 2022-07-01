47216c5eb29f43043483b6e0a10f19e8a6cf5a14
otuwomi <otuwomi@gmail.com>
2022-07-01 01:53:50 -0700

    for 6 correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)   
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -12,6 +12,7 @@ int main(void)
 	long long int c;
 	char d;
 	float f;
+
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
