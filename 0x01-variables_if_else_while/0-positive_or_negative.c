c64d4ab075bc1aab10abc60ce7e3b15e0f53d5db
otuwomi <otuwomi@gmail.com>
2022-07-05 10:29:02 -0700

    again

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,21 +2,21 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - assigns a random umber to int n everytime 
+ * main - assigns a random umber to int n everytime
  * it executes, and prints it
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n > 0)
+	if (n > 0)
 		printf("%d is positive\n",  n);
-	else if(n == 0)
+	else if (n == 0)
 		printf("%d is zero\n", n);
-	else if(n < 0)
+	else if (n < 0)
 		printf("%d is negative\n", n);
 		return (0);
-	
 }
