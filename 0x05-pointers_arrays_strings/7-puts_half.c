6009301e2bb830f2d7174ee97a61e67462137370
otuwomi <otuwomi@gmail.com>
2022-07-13 02:00:42 -0700

    for task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - prints a string
+ * @str: points to pointer
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len, n, i;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; str[i] != '\0'; -++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else if (len % 2)
+	{
+		for (len - 1) / 2; n < len - 1; n++)
+		{
+			_putcahr(str[n + 1]);
+		}
+	}
+	putchar('\n');
+}
