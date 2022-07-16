dff9995c4e9e599e075edf209053a0e4aa3628f3
otuwomi <otuwomi@gmail.com>
2022-07-16 06:48:44 -0700

    fix task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,16 +9,16 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int dest_length, i;
+	int dest_leng, i;
 
-	for (dest_length, = 0; dest[dest_length] != '\0'; dest_length++)
+	for (dest_leng, = 0; dest[dest_leng] != '\0'; dest_leng++)
 	;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[dest_length + i] = src[i];
+		dest[dest_leng + i] = src[i];
 
-	/*ending with a end of string char*/
-	dest[dest_length + i] = '\0';
+	/*should end with a end of string char*/
+	dest[dest_leng + i] = '\0';
 
 	return (dest);
 }
