ef03d73edf30bae81b239813c286405ef0b30e3b
otuwomi <otuwomi@gmail.com>
2022-07-16 21:02:46 -0700

    fix

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -13,7 +13,7 @@ int main(void)
 	 * Remember:
 	 * - you are not allowed to use a
 	 */
-	*(p + 5) = 98;
+	p[5] = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
