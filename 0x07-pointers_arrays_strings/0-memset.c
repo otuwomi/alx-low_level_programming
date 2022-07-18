a890ed7fd4b6de3833dd0474784e3045ec1da9ad
otuwomi <otuwomi@gmail.com>
2022-07-18 13:35:43 -0700

    task 1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,20 @@
-int _putchar(char c);
+#include "main.h"
 
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
-void set_string(char **s, char *to);
+/**
+ * _memset - fills n bytes
+ * @s: pointer to the char parameters
+ * @b: data to change
+ * @n: index
+ * Return: *s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
