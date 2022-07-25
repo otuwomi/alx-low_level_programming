44aa3a8a3e97cbabb4d8f0d4afdbdd37a16f66ef
otuwomi <otuwomi@gmail.com>
2022-07-25 07:26:01 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - The main function
+ * @argc: argument Count of command-lines
+ * @argv: argument Vector listing the command lines
+ * Return: 0 on success
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("The name of the program is %s\n", *argv);
+	return (0);
+}
