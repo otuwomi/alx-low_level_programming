bd21f6fa30b1a28dfabbdb3b8fa0fe64686e8990
otuwomi <otuwomi@gmail.com>
2022-07-25 07:35:00 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - will be printing its name, followed by a new line
+ * @argc: argument count
+ * @argv: argument listing
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	if (argc > 0)
+	{
+		printf("%d\n", argc - 1);
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
