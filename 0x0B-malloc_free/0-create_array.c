e60a32f3f8d3ce1397178d7556a3975e735255fa
otuwomi <otuwomi@gmail.com>
2022-07-26 09:53:16 -0700

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * create_array -> creating array list
+ * @size: size
+ * @c: character
+ * Return: return a pointer
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *s;
+
+	if (size == 0)
+		return (NULL);
+	s = (char *)malloc(size * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		s[i] = c;
+
+	return (s);
+	}
