91a70b5d074818f5ba74211eb67b7a53689038e4
otuwomi <otuwomi@gmail.com>
2022-07-29 01:28:40 -0700

    fix task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,19 +30,21 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		n = ls2;
 
 	lsout = ls1 + n;
+
 	sout = malloc(lsout + 1);
 
 	if (sout == NULL)
 		return (NULL);
 
-	if (i < ls1)
-	{
-		sout[i] = s1[i];
-	}
-	else
-	{
-		sout[i] = s2[i - ls1];
-	}
+	for (i = 0; i < lsout; i++)
+		if (i < ls1)
+		{
+			sout[i] = s1[i];
+		}
+		else
+		{
+			sout[i] = s2[i - ls1];
+		}
 
 	sout[i] = '\0';
 
