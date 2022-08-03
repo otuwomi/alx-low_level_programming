3c8b3ac5bd8873ae6db525daed9412f81e1dbfe3
otuwomi <otuwomi@gmail.com>
2022-08-03 02:27:04 -0700

    fix

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,12 +6,12 @@
  * @argv: arg array
  * Return: 0 on success
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
 
-		return (0);
+	return (0);
 }
